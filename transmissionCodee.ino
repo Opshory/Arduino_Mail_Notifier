08d5f76f72f6f1a2e1ccf80c61873d8ef95333bd
Opshory <zannatmondol74@gmail.com>
2017-09-28 13:29:11 +0600

    Add files via upload

---
+++ transmissionCodee.ino
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+int threshold = 350;
+
+int sensorPin = A0;
+int sensorValue = 0;
+int temp;
+int alarm = 2;
+
+void setup() 
+{
+  Serial.begin(9600);
+  temp = 0;
+  pinMode(alarm, OUTPUT);
+}
+
+void loop() 
+{
+ sensorValue = analogRead(sensorPin);
+ Serial.println(sensorValue);
+ if (sensorValue < threshold)
+  {
+    digitalWrite(alarm, HIGH);
+    Serial.println("Mail !!! Mail !!! Mail !!!");
+    delay(3000);
+  }
+         
+ else
+  {
+   
+    digitalWrite(alarm, LOW);
+    delay(500);
+  } 
+}
